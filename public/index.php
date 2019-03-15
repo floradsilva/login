de39e24daf38bc800bc8c7ee3faf6bb38ff17f59
flora.dsouza <flora.dsouza@wisdmlabs.com>
2019-03-15 19:03:55 +0530

    adding htaccess and basic routing code

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-//handle your routing code here
+$request = $_SERVER['REDIRECT_URL'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        require __DIR__ . '/views/404.php';
+        break;
+}
