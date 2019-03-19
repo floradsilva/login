f767d922bcebb0e95277765688ef273d1cc2f2c4
flora.dsouza <flora.dsouza@wisdmlabs.com>
2019-03-19 12:33:53 +0530

    adding templates for head, header, footer

--- public/views/index.php
+++ public/views/index.php
@@ -1,61 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>                        
-            </button>
-            <a class="navbar-brand" href="#">Logo</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-            <!-- <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul> -->
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
-            </div>
-        </div>
-    </nav>
-    
-    <div class="container text-center">    
-        <div class="row content">
-            <div class="col-sm-12 text-left"> 
-                <h1>Article</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum vehicula est vitae sollicitudin. Ut pulvinar ultricies posuere. Integer ac iaculis dui, vel rhoncus erat. In tortor nisl, vulputate quis ante vel, vestibulum luctus nunc. Phasellus id lectus ante. Vivamus ultrices, nunc ut sollicitudin hendrerit, leo tellus lacinia nulla, at porta orci orci eget eros. Nullam id ullamcorper mauris, sit amet cursus risus. Nullam a commodo metus, et facilisis velit. Maecenas volutpat mattis odio vel dignissim. Etiam ac fringilla diam, suscipit sodales ipsum. Nam in velit quam. Nunc luctus tempor leo, vel rhoncus mi mattis nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam felis massa, ultrices sit amet turpis vel, accumsan blandit est. Etiam nec eros commodo, cursus turpis pulvinar, volutpat odio.
-                </p>
-            </div>
-        </div>
-        <div class="row content">
-            <div class="col-sm-12 text-left parargraph-2"> 
-                <p id="priv">
-                    Sed vitae turpis ac nisi malesuada blandit. Quisque eu molestie eros. Donec facilisis hendrerit augue, eu adipiscing sem lacinia non. Integer sodales purus  dio, non pharetra massa accumsan in. In vitae nunc non erat posuere tempus a id ipsum. Suspendisse enim augue, bibendum eget nunc in, lacinia ultrices tortor. Nam enim lorem, gravida eget varius eu, euismod et purus. Etiam egestas sem eu nisi porttitor pharetra. Donec eu libero eu lorem convallis porta. Nullam interdum vitae lorem sit amet dignissim. Proin sit amet tortor ac odio varius dapibus nec at Sem.
-                </p>
-                <p id="login-btn">To read more, please 
-                    <a href="#">Login</a> or <a href="#">Register</a>
-                </p>
-            </div>
+<?php include_once "templates/head.php"; ?>
+<?php include_once "templates/header.php"; ?>
+<div class="container text-center">    
+    <div class="row content">
+        <div class="col-sm-12 text-left"> 
+            <h1>Article</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum vehicula est vitae sollicitudin. Ut pulvinar ultricies posuere. Integer ac iaculis dui, vel rhoncus erat. In tortor nisl, vulputate quis ante vel, vestibulum luctus nunc. Phasellus id lectus ante. Vivamus ultrices, nunc ut sollicitudin hendrerit, leo tellus lacinia nulla, at porta orci orci eget eros. Nullam id ullamcorper mauris, sit amet cursus risus. Nullam a commodo metus, et facilisis velit. Maecenas volutpat mattis odio vel dignissim. Etiam ac fringilla diam, suscipit sodales ipsum. Nam in velit quam. Nunc luctus tempor leo, vel rhoncus mi mattis nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam felis massa, ultrices sit amet turpis vel, accumsan blandit est. Etiam nec eros commodo, cursus turpis pulvinar, volutpat odio.
+            </p>
         </div>
     </div>
-        <footer class="container-fluid text-center">
-        <p>Copyright <span>&copy; </span>2019 Wisdmlabs All rights reserved.</p>
-    </footer>
-</body>
-</html>
+    <div class="row content">
+        <div class="col-sm-12 text-left parargraph-2"> 
+            <p id="priv">
+                Sed vitae turpis ac nisi malesuada blandit. Quisque eu molestie eros. Donec facilisis hendrerit augue, eu adipiscing sem lacinia non. Integer sodales purus  dio, non pharetra massa accumsan in. In vitae nunc non erat posuere tempus a id ipsum. Suspendisse enim augue, bibendum eget nunc in, lacinia ultrices tortor. Nam enim lorem, gravida eget varius eu, euismod et purus. Etiam egestas sem eu nisi porttitor pharetra. Donec eu libero eu lorem convallis porta. Nullam interdum vitae lorem sit amet dignissim. Proin sit amet tortor ac odio varius dapibus nec at Sem.
+            </p>
+            <p id="login-btn">To read more, please 
+                <a href="#">Login</a> or <a href="#">Register</a>
+            </p>
+        </div>
+    </div>
+</div>
+<?php include_once "templates/footer.php"; ?>
+<?php include_once "templates/end-html.php"; ?>
+
---
+++ public/views/templates/footer.php
@@ -0,0 +1,3 @@
+<footer class="container-fluid text-center">
+    <p>Copyright <span>&copy; </span>2019 Wisdmlabs All rights reserved.</p>
+</footer>
